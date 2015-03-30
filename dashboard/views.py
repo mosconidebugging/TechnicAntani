610ac149f0f862a8d8e7326c4f8a62136cc8d204
Radu Andries <me@admiral0.it>
2015-03-30 20:45:15 +0200

    Add venv pygit and detection of degradation

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from cachebuilder.pack_manager import ModpackManager
 from api.models import ModpackCache
 from api.views import get_root_for
+import pygit2
 
 @login_required
 def index(request):
-    pm = ModpackManager()
     context = {
         "packs": ModpackCache.objects.all(),
-        "rootdir": get_root_for(request, "", "")
+        "rootdir": get_root_for(request, "", ""),
+        "git_ssh": pygit2.features < 4,
+        "git_https": pygit2.features < 2
     }
     return render(request, "dashboard/index.html", context)
